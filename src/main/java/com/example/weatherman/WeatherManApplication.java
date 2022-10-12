71f68f91fce6e2d26c73dba48382942d01ae6808
Hyunebin <qortjr83@naver.com>
2022-10-13 04:50:06 +0900

    Initial commit

---
+++ src/main/java/com/example/weatherman/WeatherManApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherman/WeatherManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
